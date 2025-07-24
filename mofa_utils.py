c47c0b1b665a479e62f76767a4be774788918757
syntactic <timothyakho@gmail.com>
2025-07-24 15:39:40 -0400

    Added StandardScaler to MOFA processing

--- mofa_utils.py
+++ mofa_utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 # We can still import helpers if they are independent of the PCA pipeline
 from analysis_utils import clean_rsi, clean_switch_rate, classify_paradigm
+from sklearn.preprocessing import StandardScaler
 
 # 1. Define Mappings
 VIEW_MAPPING = {
@@ -32,6 +33,9 @@ CATEGORICAL_ENCODING = {
     # ... add mappings for SRM, TCT, etc.
 }
 
+NUMERICAL_COLS = ['Inter-task SOA', 'Distractor SOA', 'Task 1 CSI', 'Task 2 CSI', 'RSI',
+'Switch Rate', 'Task 1 Difficulty', 'Task 2 Difficulty', 'Task 2 Response Probability']
+
 def preprocess_for_mofa(df_raw):
     """
     Transforms the raw, wide-format design space CSV into a long-format
@@ -45,10 +49,20 @@ def preprocess_for_mofa(df_raw):
     if 'RSI Is Predictable' in df.columns:
             df['RSI Is Predictable'] = df['RSI Is Predictable'].apply(lambda x: 1.0 if str(x).lower() == 'yes' else 0.0)
 
-    # --- Step 2: Numerical Encoding of Categoricals ---
+    # --- Step 2: Numerical Encoding of Categoricals, Normalization of Numerics ---
     for col, mapping in CATEGORICAL_ENCODING.items():
         if col in df.columns:
             df[col] = df[col].map(mapping)
+
+    df_numeric = df[[col for col in NUMERICAL_COLS if col in df.columns]]
+
+    scaler = StandardScaler()
+    df_scaled_numeric = pd.DataFrame(scaler.fit_transform(df_numeric), columns=df_numeric.columns, index=df.index)
+
+    # Update the main DataFrame with the scaled values
+    df_before_scaling = df.copy()
+    df.update(df_scaled_numeric)
+
 
     # --- Step 3: Melt DataFrame from Wide to Long ---
     id_vars = ['Experiment'] # The column that identifies a 'sample'
@@ -60,7 +74,7 @@ def preprocess_for_mofa(df_raw):
     # --- Step 4: Add 'view' and 'group' columns ---
     df_long['view'] = df_long['feature'].map(FEATURE_TO_VIEW)
     # Use paradigm classification for the 'group' column
-    df_temp_for_group = df.set_index('Experiment')
+    df_temp_for_group = df_before_scaling.set_index('Experiment')
     df_long['group'] = df_long['Experiment'].apply(lambda x: classify_paradigm(df_temp_for_group.loc[x]))
     df_long.rename(columns={'Experiment': 'sample'}, inplace=True)
     
